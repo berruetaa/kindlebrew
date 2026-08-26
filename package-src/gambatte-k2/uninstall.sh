8faa61a96ac3fa3069bd90afd432316eba69f36a
Berrueta <berruetx@gmail.com>
2026-08-26 09:00:59 -0300

    feat: add Gambatte-K2 uninstall script

---
+++ package-src/gambatte-k2/uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -eu
+rm -f /mnt/us/documents/Gambatte-K2.sh
+rm -rf /mnt/us/extensions/gambatte-k2 /mnt/us/extensions/gambatte-k2.kpm-new
+echo "Gambatte-K2 removed. ROMs stored elsewhere are untouched."
