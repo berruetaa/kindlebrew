99e714908e8cfea76be9aee2065acdd7f730cc71
Berrueta <berruetx@gmail.com>
2026-08-29 14:26:38 -0300

    engine: poll external fds on host

--- engine/src/kb_backend_headless.c
+++ engine/src/kb_backend_headless.c
@@ -3,6 +3,8 @@
  */
 #include "kb_internal.h"
 
+#include <errno.h>
+#include <poll.h>
 #include <string.h>
 
 static int headless_init(KBGame *game) {
@@ -30,8 +32,52 @@ static int headless_present(KBGame *game, const KBRect *rects, int count, KBRefr
     return 0;
 }
 
+static unsigned fd_flags(short revents) {
+    unsigned flags = 0;
+    if (revents & (POLLIN | POLLPRI)) flags |= KB_FD_READABLE;
+    if (revents & POLLHUP) flags |= KB_FD_HANGUP;
+    if (revents & POLLERR) flags |= KB_FD_ERROR;
+    if (revents & POLLNVAL) flags |= KB_FD_INVALID;
+    return flags;
+}
+
 static int headless_poll(KBGame *game, KBEvent *event, int timeout_ms) {
-    (void)game; (void)timeout_ms;
+    struct pollfd pfds[KB_MAX_FD_WATCHES];
+    int watch_index[KB_MAX_FD_WATCHES];
+    int n = 0;
+
+    for (int i = 0; i < KB_MAX_FD_WATCHES; ++i) {
+        if (!game->fd_watches[i].active) continue;
+        pfds[n].fd = game->fd_watches[i].fd;
+        pfds[n].events = POLLIN | POLLPRI;
+        pfds[n].revents = 0;
+        watch_index[n] = i;
+        ++n;
+    }
+
+    int rc = poll(pfds, (nfds_t)n, timeout_ms);
+    if (rc < 0) {
+        if (errno == EINTR) return 0;
+        kb_set_error(game, "poll(external fd) failed");
+        return -1;
+    }
+    if (rc == 0) {
+        memset(event, 0, sizeof(*event));
+        return 0;
+    }
+
+    for (int p = 0; p < n; ++p) {
+        unsigned flags = fd_flags(pfds[p].revents);
+        if (!flags) continue;
+        KBFdWatch *watch = &game->fd_watches[watch_index[p]];
+        memset(event, 0, sizeof(*event));
+        event->type = KB_EVENT_FD;
+        event->time_ms = kb_now_ms();
+        event->id = watch->id;
+        event->value = (int)flags;
+        return 1;
+    }
+
     memset(event, 0, sizeof(*event));
     return 0;
 }
